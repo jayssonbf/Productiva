227838bda94a13a7f8506b64a4edbf6f33b26bc0
jayssonbf <jayssonbf@outlook.com>
2020-09-05 13:24:40 -0400

    ActController added

---
+++ src/main/java/ActController.java
@@ -0,0 +1,14 @@
+import javafx.event.ActionEvent;
+
+public class ActController {
+
+  public void buttonAction(ActionEvent event){
+    System.out.println("Hello User, the product has been added!");
+  }
+
+  public void recProd(ActionEvent actionEvent) {
+
+    System.out.println("System.out.println");
+  }
+
+}
